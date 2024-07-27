78b9b69de20ca7b3089de2a54e6ce5b0cab63ba4
Anyungu Wanyungu <anyungucw@gmail.com>
2024-07-27 14:14:33 +0300

    test header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import HeaderBalance from "./HeaderBalance";
+import { getAccounts } from "@/db/db.util";
+import ClientStoreInitializer from "@/components/ClientStoreInitializer";
+
+export const revalidate = 0;
 
 type Props = {};
 
-const Header = ({}: Props) => {
+const Header = async ({}: Props) => {
+  const accounts = await getAccounts();
+  <ClientStoreInitializer accounts={accounts} />;
   return (
     <header className=" sticky top-2 bg-red flex flex-row justify-around py-4">
       <HeaderBalance />
