f97d18d4a77c47fb3abef5d31d3b2413c7af7d5d
Anyungu Wanyungu <anyungucw@gmail.com>
2024-10-17 21:19:07 +0300

    test company create

--- components/ClientStoreInitializer.tsx
+++ components/ClientStoreInitializer.tsx
@@ -4,7 +4,7 @@ import { useAccountStore } from "@/store/account.store";
 import React, { useEffect } from "react";
 
 type Props = {
-  accounts: Account[];
+  accounts: BalanceAccount[];
 };
 
 function ClientStoreInitializer({ accounts }: Props) {
